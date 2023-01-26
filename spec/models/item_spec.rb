a152fcfcb77f66e9c1445b946b9d1f19462ff2b9
Samuel Cox <samc1253@gmail.com>
2023-01-26 09:48:26 -0700

    Create model tests and bases of controllers

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Associations' do
+    it { should belong_to(:todo) }
+  end
+
+  describe 'Validations' do
+    it { should validate_presence_of(:name) }
+  end
 end
